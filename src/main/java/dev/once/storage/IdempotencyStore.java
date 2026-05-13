28d85887ddc11d8dac2c5a1e8acf86b73c24b0bf
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-13 18:45:06 +0300

    Add `JsonProcessingException` to `IdempotencyStore#set` method signature

--- src/main/java/dev/once/storage/IdempotencyStore.java
+++ src/main/java/dev/once/storage/IdempotencyStore.java
@@ -1,7 +1,9 @@
 package dev.once.storage;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public interface IdempotencyStore {
-    void set(String key,IdempotentResponse idempotentResponse);
+    void set(String key,IdempotentResponse idempotentResponse) throws JsonProcessingException;
     IdempotentResponse get(String key);
     void delete(String key);
     void setInFlight(String key);
