b44a3d518d48d5299a8b6e3e4a8ea0916ac30a11
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-13 23:06:08 +0300

    Refactor `IdempotencyAutoConfiguration` to simplify configuration and import `RedisIdempotencyConfiguration`

--- src/main/java/dev/once/configuration/IdempotencyAutoConfiguration.java
+++ src/main/java/dev/once/configuration/IdempotencyAutoConfiguration.java
@@ -1,55 +1,30 @@
 package dev.once.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.once.filter.IdempotencyFilter;
 import dev.once.filter.WrapperFilter;
 import dev.once.storage.IdempotencyStore;
 import dev.once.storage.InMemoryIdempotencyStore;
-import dev.once.storage.RedisIdempotencyStore;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @AutoConfiguration
 @EnableConfigurationProperties(IdempotencyProperties.class)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@Import( RedisIdempotencyConfiguration.class)
 public class IdempotencyAutoConfiguration {
     @Bean
-    @ConditionalOnProperty(prefix = "idempotency",
-            name = "store",
-            havingValue = "in-memory")
     @ConditionalOnMissingBean({IdempotencyStore.class, RedisConnectionFactory.class})
     public IdempotencyStore inMemoryIdempotencyStore(IdempotencyProperties properties) {
         return new InMemoryIdempotencyStore(properties.getTtl().getSeconds());
     }
 
-    @Bean("idempotencyRedisTemplate")
-    @ConditionalOnProperty(prefix = "idempotency",
-            name = "store",
-            havingValue = "redis")
-    @ConditionalOnBean(RedisConnectionFactory.class)
-    public RedisTemplate<String,String > redisIdempotencyStore(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
-    }
-    @Bean
-    @ConditionalOnBean(RedisConnectionFactory.class)
-    public IdempotencyStore redisIdempotencyStore(
-            @Qualifier("idempotencyRedisTemplate") RedisTemplate<String, String> template,
-            ObjectMapper objectMapper,
-            IdempotencyProperties properties) {
-        return new RedisIdempotencyStore(template, objectMapper, properties.getTtl().toSeconds());
-    }
     @Bean
     public IdempotencyFilter idempotencyFilter(IdempotencyStore idempotencyStore) {
         return new IdempotencyFilter(idempotencyStore);
@@ -57,6 +32,15 @@ public class IdempotencyAutoConfiguration {
     @Bean
     public WrapperFilter wrapperFilter(IdempotencyStore idempotencyStore) {
         return new WrapperFilter(idempotencyStore);
+    }
+    @Bean
+    public WebMvcConfigurer idempotencyWebMvcConfigurer(IdempotencyFilter idempotencyFilter) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(idempotencyFilter);
+            }
+        };
     }
 
 
