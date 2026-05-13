bd84405c876448c22f39156c797145cbee7542a7
melbiialy <mahmoudelbialy109@gmail.com>
2026-05-13 21:17:36 +0300

    Add `WrapperFilter` for handling idempotency with response caching

---
+++ src/main/java/dev/once/filter/WrapperFilter.java
@@ -0,0 +1,45 @@
+package dev.once.filter;
+
+import dev.once.storage.IdempotencyStore;
+import dev.once.storage.IdempotentResponse;
+import dev.once.storage.Status;
+import dev.once.wrapper.ContentCachingResponseWrapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WrapperFilter extends OncePerRequestFilter {
+    private final IdempotencyStore idempotencyStore;
+
+    public WrapperFilter(IdempotencyStore idempotencyStore) {
+        this.idempotencyStore = idempotencyStore;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        try {
+            filterChain.doFilter(request, responseWrapper);
+        }finally {
+            String key = (String) request.getAttribute("idempotency-key");
+            if (key!=null){
+                byte[] body = responseWrapper.getContent();
+                Map<String,String> headers = new HashMap<>();
+                responseWrapper.getHeaderNames().forEach(name -> headers.put(name, responseWrapper.getHeader(name)));
+                idempotencyStore.set(key,new IdempotentResponse(response.getStatus(),body,
+                        response.getContentType(), Instant.now(),headers, Status.COMPLETED));
+            }
+
+        }
+    }
+}
